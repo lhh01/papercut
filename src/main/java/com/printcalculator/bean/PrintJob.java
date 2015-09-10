49e8b83f79926a3aa145cc8ceb1d24f6affa9006
LiH <LiH@HAULT069.hsntech.int>
2015-09-10 21:29:56 +1000

    add some members

--- src/main/java/com/printcalculator/bean/PrintJob.java
+++ src/main/java/com/printcalculator/bean/PrintJob.java
@@ -7,15 +7,69 @@ package com.printcalculator.bean;
 
 import com.printcalculator.enums.PrintJobName;
 import com.printcalculator.enums.PrintJobType;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  *
  * @author LiH
  */
-public abstract class PrintJob {
-    
-      public PrintJob(int totalPages, int colourPages, PrintJobType jobType, PrintJobName jobName) {
-       
+public abstract class PrintJob implements Serializable {
+
+    private static final long serialVersionUID = 6019011034372588061L;
+
+    private final String id = UUID.randomUUID().toString();
+    private final PrintJobType jobType;
+    private final PrintJobName jobName;
+    private final int numberOfTotalPages;
+    private final int numberOfColourPages;
+
+    public PrintJob(int totalPages, int colourPages, PrintJobType jobType, PrintJobName jobName) {
+
+        this.numberOfTotalPages = totalPages;
+        this.numberOfColourPages = colourPages;
+        this.jobType = jobType;
+        this.jobName = jobName;
     }
-    
+
+    public PrintJobName getJobName() {
+        return jobName;
+    }
+
+    public int getNumberOfTotalPages() {
+        return numberOfTotalPages;
+    }
+
+    public int getNumberOfColourPages() {
+        return numberOfColourPages;
+    }
+
+    public PrintJobType getJobType() {
+        return jobType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.getClass().getName()).append("[id=").append(id).append("]");
+        return builder.toString();
+    }
+
 }
