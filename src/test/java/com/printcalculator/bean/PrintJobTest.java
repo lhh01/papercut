b15a133a8364ad1fba04d4516d512ad4eb034c36
LiH <LiH@HAULT069.hsntech.int>
2015-09-10 21:37:38 +1000

    refactoring test

--- src/test/java/com/printcalculator/bean/PrintJobTest.java
+++ src/test/java/com/printcalculator/bean/PrintJobTest.java
@@ -7,6 +7,7 @@ package com.printcalculator.bean;
 
 import com.printcalculator.enums.PrintJobName;
 import com.printcalculator.enums.PrintJobType;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
@@ -23,6 +24,15 @@ public abstract class PrintJobTest {
     public void testConstructor() {
 
         setInstance(0, 0, PrintJobType.Unknown, PrintJobName.Unknown);
+
+        PrintJobType type = instance.getJobType();
+        PrintJobName name = instance.getJobName();
+        int totalPageNumber = instance.getNumberOfTotalPages();
+        int colourPageNumber = instance.getNumberOfColourPages();
+        assertEquals(PrintJobType.Unknown, type);
+        assertEquals(PrintJobName.Unknown, name);
+        assertEquals(0, totalPageNumber);
+        assertEquals(0, colourPageNumber);
 
     }
 
