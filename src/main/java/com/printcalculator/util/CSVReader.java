b6219daf5ad904ba058e94ae6be28cd8d6318b94
LiH <LiH@HAULT069.hsntech.int>
2015-09-11 21:28:55 +1000

    refactoring test & implementation

--- src/main/java/com/printcalculator/util/CSVReader.java
+++ src/main/java/com/printcalculator/util/CSVReader.java
@@ -5,6 +5,10 @@
  */
 package com.printcalculator.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +17,36 @@ import java.util.List;
  */
 public class CSVReader {
 
-    List<String[]> readAll() {
-        return null;
+    private static final String DEFAULT_CSV_SEPARATOR = ",";
+    private BufferedReader reader;
+    private String csvSeparator;
+
+    public CSVReader(Reader reader) {
+        this(reader, DEFAULT_CSV_SEPARATOR);
+    }
+
+    public CSVReader(Reader reader, String csvSeparator) {
+        this.reader = (reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader));
+
+        this.csvSeparator = csvSeparator;
+    }
+
+    List<String[]> readAll() throws IOException {
+        String line;
+        ArrayList<String[]> entries = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+
+            String[] entry = line.split(csvSeparator);
+
+            entries.add(entry);
+
+        }
+
+        return entries;
+    }
+
+    public void close() throws IOException {
+        reader.close();
     }
 }
--- src/test/java/com/printcalculator/util/CSVReaderTest.java
+++ src/test/java/com/printcalculator/util/CSVReaderTest.java
@@ -6,6 +6,12 @@
 package com.printcalculator.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,10 +30,13 @@ public class CSVReaderTest {
 
     private static final Logger logger = Logger.getLogger(CSVReaderTest.class.getName());
     private static final String FILE_SEPARATOR = File.separator;
-    
+
     private static final String CSV_FILE_NAME = "printjobs.csv";
 
+    private FileInputStream fileInputStream;
+    private InputStreamReader inputStreamReader;
     private CSVReader csvReader;
+
     public CSVReaderTest() {
     }
 
@@ -40,11 +49,44 @@ public class CSVReaderTest {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws FileNotFoundException, UnsupportedEncodingException {
+
+        String sampleFolderPath = Paths.get("sample_data").toAbsolutePath().toString();
+
+        File inputFile = new File(sampleFolderPath + FILE_SEPARATOR + CSV_FILE_NAME);
+
+        fileInputStream = new FileInputStream(inputFile);
+        inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+
+        csvReader = new CSVReader(inputStreamReader);
     }
 
     @After
     public void tearDown() {
+
+        if (csvReader != null) {
+            try {
+                csvReader.close();
+            } catch (IOException e) {
+                logger.log(Level.FINE, "cannot close csvReader");
+            }
+        }
+
+        if (inputStreamReader != null) {
+            try {
+                inputStreamReader.close();
+            } catch (IOException e) {
+                logger.log(Level.FINE, "cannot close inputStreamReader");
+            }
+        }
+
+        if (fileInputStream != null) {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                logger.log(Level.FINE, "cannot close fileInputStream");
+            }
+        }
     }
 
     @Test
