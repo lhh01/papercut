1f2b887d3150c246f3338a6973d3e6ca2fce36dc
LiH <LiH@HAULT069.hsntech.int>
2015-09-12 20:15:45 +1000

    code refactoring

--- src/main/java/com/printcalculator/core/A4PrintJobProcessor.java
+++ src/main/java/com/printcalculator/core/A4PrintJobProcessor.java
@@ -6,27 +6,37 @@
 package com.printcalculator.core;
 
 import com.printcalculator.bean.PrintJob;
+import java.io.File;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  *
  * @author LiH
  */
-public class A4PrintJobProcessor extends PrintJobProcessor{
+public class A4PrintJobProcessor extends PrintJobProcessor {
+
+    private static final Logger logger = Logger.getLogger(A4PrintJobProcessor.class.getName());
+
+    private final File a4PrintJobFile;
+
+    public A4PrintJobProcessor(File printJobFile) {
+        this.a4PrintJobFile = printJobFile;
+    }
 
     @Override
     public void processPrintJob() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     protected void outputPrintJobResult(List<PrintJob> jobs) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     protected List<PrintJob> parseCsvToPrintJobList(List<String[]> entries) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
 }
--- src/test/java/com/printcalculator/core/A4PrintJobProcessorTest.java
+++ src/test/java/com/printcalculator/core/A4PrintJobProcessorTest.java
@@ -5,7 +5,15 @@
  */
 package com.printcalculator.core;
 
-
+import com.printcalculator.bean.A4DoubleSidedPrintJob;
+import com.printcalculator.bean.A4SingleSidedPrintJob;
+import com.printcalculator.bean.PrintJob;
+import com.printcalculator.enums.PrintJobName;
+import com.printcalculator.enums.PrintJobType;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,6 +26,15 @@ import static org.junit.Assert.*;
  * @author LiH
  */
 public class A4PrintJobProcessorTest extends PrintJobProcessorTest {
+
+    private static final String FILE_SEPARATOR = File.separator;
+
+    private static final String CSV_FILE_NAME = "printjobs.csv";
+
+    private File csvFile;
+
+    private A4DoubleSidedPrintJob a4DoubleSidedPrintJob;
+    private A4SingleSidedPrintJob a4SingleSidedPrintJob;
 
     public A4PrintJobProcessorTest() {
     }
@@ -34,6 +51,11 @@ public class A4PrintJobProcessorTest extends PrintJobProcessorTest {
     @Override
     public void setUp() {
         super.setUp();
+        String sampleFolderPath = Paths.get("sample_data").toAbsolutePath().toString();
+        csvFile = new File(sampleFolderPath + FILE_SEPARATOR + CSV_FILE_NAME);
+
+        a4DoubleSidedPrintJob = new A4DoubleSidedPrintJob(100, 30, PrintJobName.A4DoubleSide);
+        a4SingleSidedPrintJob = new A4SingleSidedPrintJob(100, 0, PrintJobName.A4SingleSide);
     }
 
     @After
@@ -44,7 +66,45 @@ public class A4PrintJobProcessorTest extends PrintJobProcessorTest {
 
     @Override
     protected void setInstance() {
-        instance = new A4PrintJobProcessor();
+        instance = new A4PrintJobProcessor(csvFile);
+    }
+
+    @Test
+    public void testParseCsvToPrintJobList() {
+
+        List<String[]> entries = new ArrayList<>();
+        String[] row1 = {"100", "30", "true"};
+        String[] row2 = {"100", "0", "false"};
+        entries.add(row1);
+        entries.add(row2);
+
+        A4PrintJobProcessor jobProcessor = new A4PrintJobProcessor(csvFile);
+
+        List<PrintJob> result = jobProcessor.parseCsvToPrintJobList(entries);
+        assertTrue(result.size() == 2);
+        PrintJob firstJob = result.get(0);
+        PrintJob secondJob = result.get(1);
+
+        PrintJobType firstType = firstJob.getJobType();
+        PrintJobName firstName = firstJob.getJobName();
+        int firstTotalPageNumber = firstJob.getNumberOfTotalPages();
+        int firstColourPageNumber = firstJob.getNumberOfColourPages();
+
+        assertTrue(a4DoubleSidedPrintJob.getJobType() == firstType);
+        assertTrue(PrintJobName.A4DoubleSide == firstName);
+        assertEquals(100, firstTotalPageNumber);
+        assertEquals(30, firstColourPageNumber);
+
+        PrintJobType secondType = secondJob.getJobType();
+        PrintJobName secondName = secondJob.getJobName();
+        int secondTotalPageNumber = secondJob.getNumberOfTotalPages();
+        int secondColourPageNumber = secondJob.getNumberOfColourPages();
+
+        assertTrue(a4SingleSidedPrintJob.getJobType() == secondType);
+        assertTrue(PrintJobName.A4SingleSide == secondName);
+        assertEquals(100, secondTotalPageNumber);
+        assertEquals(0, secondColourPageNumber);
+
     }
 
 }
