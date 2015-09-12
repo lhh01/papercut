2b118a54f23a7e09e0bf8c199186bc43d53fe36d
LiH <LiH@HAULT069.hsntech.int>
2015-09-12 12:29:38 +1000

    add more tests

--- src/test/java/com/printcalculator/util/PrintJobFactoryTest.java
+++ src/test/java/com/printcalculator/util/PrintJobFactoryTest.java
@@ -8,6 +8,7 @@ package com.printcalculator.util;
 import com.printcalculator.bean.PrintJob;
 import com.printcalculator.enums.PrintJobName;
 import com.printcalculator.enums.PrintJobType;
+import com.printcalculator.exception.UnexpectedException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -16,6 +17,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -24,6 +27,9 @@ import static org.junit.Assert.*;
 public class PrintJobFactoryTest {
 
     private static final Logger logger = Logger.getLogger(PrintJobFactoryTest.class.getName());
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     public PrintJobFactoryTest() {
     }
@@ -59,6 +65,37 @@ public class PrintJobFactoryTest {
         assertEquals(jobName, jobNameofGeneratedJob);
         assertEquals(25, totalPageNumberOfGeneratedJob);
         assertEquals(10, colourPageNumberOfGeneratedJob);
+
+    }
+
+    @Test
+    public void testGetA4DoubleSidedPrintJob() {
+        logger.log(Level.FINE, "testGetA4DoubleSidedPrintJob...");
+        PrintJobType jobType = PrintJobType.A4DoubleSide;
+        PrintJobName jobName = PrintJobName.A4DoubleSide;
+
+        PrintJob result = PrintJobFactory.getA4PrintJob(jobType, 55, 13);
+        PrintJobType jobTypeofGeneratedJob = result.getJobType();
+        PrintJobName jobNameofGeneratedJob = result.getJobName();
+        int totalPageNumberOfGeneratedJob = result.getNumberOfTotalPages();
+        int colourPageNumberOfGeneratedJob = result.getNumberOfColourPages();
+        assertEquals(jobType, jobTypeofGeneratedJob);
+        assertEquals(jobName, jobNameofGeneratedJob);
+        assertEquals(55, totalPageNumberOfGeneratedJob);
+        assertEquals(13, colourPageNumberOfGeneratedJob);
+
+    }
+
+    @Test
+    public void testGetA4PrintJobWithUnknownTypeException() {
+        logger.log(Level.FINE, "testGetA4PrintJobWithUnknownTypeException...");
+
+        thrown.expect(UnexpectedException.class);
+        thrown.expectMessage("Cannot create a print job ojbect. Unknow print job type");
+
+        PrintJobType jobType = PrintJobType.Unknown;
+
+        PrintJob result = PrintJobFactory.getA4PrintJob(jobType, 55, 13);
 
     }
 
